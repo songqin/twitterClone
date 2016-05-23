2a1ff0516fe303e366c3975d64b678bc88bb44e4
Song Qin <richieqin@gmail.com>
2016-05-23 02:56:44 -0400

    a

--- public/app.js
+++ public/app.js
@@ -1,12 +1,41 @@
-var app = angular.module('twitterCloneAppp', []);
+var app = angular.module("twitterCloneApp", ["ngRoute"]);
 
-app.controller('mainController', function($scope){
-  $scope.posts = [];
-  $scope.newPost = {created_by: '', text: '', created_at: ''};
-
-  $scope.post = function(){
-    $scope.newPost.created_at = Date.now();
-    $scope.posts.push($scope.newPost);
-    $scope.newPost = {created_by: '', text: '', created_at: ''};
-  };
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
 });
+app.controller("mainController", function($scope){
+	$scope.posts = [];
+	$scope.newPost = {createdBy:"",text:"",createdAt:""};
+
+	$scope.post = function(){
+		$scope.newPost.createdAt = Date.now();
+		$scope.posts.push($scope.newPost);
+		$scope.newPost = {createdBy:"",text:"",createdAt:""};
+	};
+
+});
+app.controller("authController", function($scope){
+	$scope.user = {username:"",password:""};
+	$scope.errorMessage="";
+	$scope.login=function(){
+		$scope.errorMessage = "login request for" +  $scope.user.username;
+	};
+	$scope.register = function(){
+		$scope.errorMessage = "register request for " + $scope.user.password;
+	};
+})
